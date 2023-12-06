4595c7887c16a73d2413d630eaea4166ae303a88
VanessaTrajano <vanessapalmeirakelmertrajano@gmail.com>
2023-12-06 20:25:22 -0300

    Cria um Event Listener

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
+document.entrada.addEventListener("submit", leFormulario())
+
 function leFormulario(){
     const fruta = document.entrada.fruta.value
     const quantidade = document.entrada.quantidade.value
 
-    console.log(fruta, quantidade)
+    console.log(`${fruta}, ${quantidade}`)
 }
