0ee2386185906cfa560b446ef951203c0016c7b5
VanessaTrajano <vanessapalmeirakelmertrajano@gmail.com>
2023-12-06 19:57:05 -0300

    Cria um objeto estoque que engloba joao e maria

--- estoque.js
+++ estoque.js
@@ -1,14 +1,5 @@
-let joao = [{
-    tipo: "maca",
-    qtd: 1
-}, {
-    tipo: "pera",
-    qtd: 1
-}]
-let maria = [{
-    tipo: "maca",
-    qtd: 2
-}, {
-    tipo: "banana",
-    qtd: 1
-}]
+let estoque = {
+    joao: [{ tipo: "maca", qtd: 1}, {tipo: "pera", qtd: 1}],
+    maria: [{tipo: "maca", qtd: 2}, {tipo: "banana", qtd: 1}]
+}
+
