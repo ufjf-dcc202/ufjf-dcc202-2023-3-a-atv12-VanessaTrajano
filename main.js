a573a7c0c1dcd389e15ac1549138cb85491150eb
VanessaTrajano <vanessapalmeirakelmertrajano@gmail.com>
2023-12-17 10:37:28 -0300

    Cria função preencheListaPessoa

--- main.js
+++ main.js
@@ -17,3 +17,15 @@ function leFormulario(event){
     transacaoNoEstoque(origem, destino, quantidade, fruta)
 }
 
+function preencheListaPessoa(pessoa, lista){
+    lista.innerHTML = ""
+
+    if(Array.isArray(pessoa)){
+        for(let i = 0; i < pessoa.length; i++){
+            const monte = pessoa[i]
+            const eLi = document.createElement('li')
+            eLi.innerText = `${monte.tipo}: ${monte.quantidade}`
+            lista.append(eLi)
+        }
+    }
+}
